9d4c76a49feb61f0e7d4ce1a31dc525bfaee7d62
Brian Brennglass <>
2021-12-21 13:56:52 -0500

    established routing

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Routes from './Components/Routes';
+
+function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+  
+  return (
+    <div className={darkTheme? 'dark': ''}>
+      <div className = "bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme = {darkTheme} />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Routes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Routes(){
+
+    return(
+        <div>
+            <h1>Routes</h1>
+        </div>
+    )
+}
